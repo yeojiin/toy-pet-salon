83058ba90f271ebd76ebc91df343684697eebe89
jylim <jylim@12cm.co.kr>
2023-03-10 13:48:15 +0900

    initial commit

---
+++ src/main/java/com/ddd/toy/pet/salon/ToyPetSalonApplication.java
@@ -0,0 +1,13 @@
+package com.ddd.toy.pet.salon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyPetSalonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyPetSalonApplication.class, args);
+    }
+
+}
